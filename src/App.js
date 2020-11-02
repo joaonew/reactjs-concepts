d0dd23fbfbed8a5d28deff3f85be323eb8be9516
João Fiche <jncfiche@gmail.com>
2020-11-01 21:51:05 -0500

    Passed both tests on route projects

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      setProjects(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      url: "Joao Fiche",
+      techs: ['Node.Js', 'React']
+    });
+    const project = response.data;
+
+    setProjects([...projects, project]);
+    
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`projects/${id}`);
+    setProjects(projects.filter(project => project.id !== id));
+
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      <ul data-testid="repository-list">   
+          {projects.map(project => (
+            <li key={project.id}>
+              {project.title}
+                <button onClick={() => handleRemoveRepository(project.id)}>
+                Remover
+                </button>
+            </li>
+          ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
